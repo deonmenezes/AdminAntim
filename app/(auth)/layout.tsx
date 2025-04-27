4a24c319cd8f9f8a51393f3477a0f703ef2f8b82
deonmenezes <150112885+deonmenezes@users.noreply.github.com>
2025-04-27 11:02:01 +0530

    stts

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Auth",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: "STTS - Admin Auth",
+  description: "Admin dashboard to manage STTS's data",
 };
 
 export default function RootLayout({
